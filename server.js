ee0865715f64a9a7eb025e401531a6371617f725
Grant Dahlhauser <BrainEmber@Gmail.com>
2017-11-08 13:55:22 -0800

    set up file structure

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-"whatever dog"
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 2080;
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, function(){
+  console.log('express listening on port ' + PORT);
+});
